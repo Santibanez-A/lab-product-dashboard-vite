ed28b74d4514267770709ae155a574c60af5c0ea
Adrian Santibanez <santibanez1996@gmail.com>
2026-06-06 15:31:49 -0500

    Complete product dashboard lab

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import styles from '../styles/ProductCard.module.css';
 
-const ProductCard = ({ product }) => {
+const ProductCard = ({ product, onRemove }) => {
   return (
-    <div className>
-      {/* TODO: Apply conditional class to <div> above for out-of-stock items */}
-      
-      {/* TODO: Display product name */}
 
-      {/* TODO: Display product price */}
+    <div className={`${styles.card} ${product.inStock ? "" : "outOfStockClass"}`}>
+      <h2>{product.name}</h2>
 
-      {/* TODO: Show if the product is in stock or out of stock */}
-      
+      <p>{product.price}</p>
+
+      <p>{product.inStock ? "In Stock" : "Out of Stock"}</p>
+
+      <button onClick={() => onRemove(product.id)}>
+        Remove
+      </button>
+
     </div>
   );
 };
